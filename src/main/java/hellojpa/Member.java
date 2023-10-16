07cb31e405733cb238f8e6da44610079aedaff59
luck2jay <awkaejay97@mju.ac.kr>
2023-10-16 13:54:16 +0900

    entity mapping

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,5 +1,7 @@
 package hellojpa;
 
+import com.sun.org.apache.bcel.internal.generic.LOR;
+
 import javax.persistence.*;
 import java.time.*;
 import java.util.Date;
@@ -7,23 +9,14 @@ import java.util.Date;
 @Entity
 public class Member {
     @Id //pk맵핑
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "name")
+    @Column(name = "name",nullable = false)
     private String username;
-    private Integer age;
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-    @Lob
-    private String description;
-    @Transient
-    private int temp;
 
     public Member() {
     }
+
 
     public Long getId() {
         return id;
@@ -41,93 +34,5 @@ public class Member {
         this.username = username;
     }
 
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getTemp() {
-        return temp;
-    }
-
-    public void setTemp(int temp) {
-        this.temp = temp;
-    }
 }
-//package hellojpa;
-//import javax.persistence.Entity;
-//import javax.persistence.Id;
-//@Entity // 꼭 넣어야, JPA가 로딩될 때 JPA가 관리하는 애인지 인식
-//public class Member {
-//
-//
-//
-//
-//
-////    @Id
-////    private Long id;
-////    private String name;
-////    //private int age;
-////
-////    //Getter, Setter …
-////
-////
-////    public Member() {
-////    }
-////
-////    public Member(Long id, String name) {
-////        this.id = id;
-////        this.name = name;
-////    }
-////
-////    public Long getId() {
-////        return id;
-////    }
-////
-////    public void setId(Long id) {
-////        this.id = id;
-////    }
-////
-////    public String getName() {
-////        return name;
-////    }
-////
-////    public void setName(String name) {
-////        this.name = name;
-////    }
-//}
+
